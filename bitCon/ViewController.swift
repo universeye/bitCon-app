f99ddde803f68cb188eb90abd4f84b70f84c5382
Terry <terrykuo501@gmail.com>
2021-05-22 14:11:55 +0800

    Initial Commit

---
+++ bitCon/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  bitCon
+//
+//  Created by Terry Kuo on 2021/5/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
